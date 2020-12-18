44d20f250334babd912ea47570b5aebe9530d147
bdruiz <bdruizr@correo.udistrital.edu.co>
2020-12-18 14:14:17 -0500

    Se añade dependencias y se pasa el proyecto a Raiz

---
+++ src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <login />
+  </div>
+</template>
+
+<script>
+import Login from '@/components/auth/login.vue'
+export default {
+  components:{
+    Login
+     
+  }  
+}
+</script>
